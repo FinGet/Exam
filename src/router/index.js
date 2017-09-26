af719a5088db2c3cb346ae4b6420eb695a918cbb
Mario <zhangbiaoFT@163.com>
2017-09-26 13:36:50 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// 前台考试
 import FrontLogin from '@/exam/login'
 import FrontHome from '@/exam/home'
+//------------ 后台管理----------------//
 import EndLogin from '@/examManage/login'
 import EndHome from '@/examManage/home'
 import Index from '@/examManage/pages/index'
+// 试卷管理
+import MyPapers from '@/examManage/pages/papersManage/mypapers'
+// 我的信息管理
+import User from '@/examManage/pages/usermsg'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // 前台考试
     { path: '/', name: 'FrontLogin', component: FrontLogin},
     { path: 'fronthome', name: 'FrontHome', component: FrontHome,
       children: [
       ]
     },
-    { path: '/managelogin', name: 'EndLogin', component: EndLogin},
-    { path: '/endhome', name: 'EndHome', component: EndHome,
+    // 后台管理
+    { path: '/managelogin', name: 'EndLogin', component: EndLogin}, //后台登录
+    { path: '/endhome', name: 'EndHome', component: EndHome, meta: {breadName:'管理系统'},
       children: [
-        { path: 'index', name: 'Index', component: Index}
+        { path: 'index', name: 'Index', component: Index, meta: {breadName:'首页'},}, // 后台首页
+        // 试卷管理
+        { path: 'mypapers', name: 'MyPapers', component: MyPapers, meta: {breadName: '我的试卷'}},
+        { path: 'user', name: 'User', component: User, meta: {breadName:'我的信息'},} // 我的信息管理
       ]
     }
   ]
