0df05acef084a1e2f712707c3d870617c62d7e61
Mario <zhangbiaoFT@163.com>
2017-09-26 13:36:26 +0800

    面包屑

--- src/components/breadnav.vue
+++ src/components/breadnav.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="bread">
       <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ name: 'Index' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        <el-breadcrumb-item v-for="(item, index) in breadData" :to="{ name: item.meta.breadName=='管理系统'?'Index':item.name }">{{item.meta.breadName}}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+  export default {
+    data() {
+      return {
+        breadData:[]
+      }
+    },
+    watch: {
+      $route () {
+        this.initBreadData();
+      }
+    },
+    methods:{
+      //面包屑
+      initBreadData(){
+        this.breadData=this.$router.currentRoute.matched;
+        console.log(this.breadData)
+      }
+    },
+    created(){
+      this.initBreadData();
+    }
+  }
 </script>
 
 <style rel="stylesheet/scss" scoped="scoped" lang="scss">
