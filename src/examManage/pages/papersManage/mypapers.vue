aa03e099fda6d25908439b45e675a9b555957357
Mario <zhangbiaoFT@163.com>
2017-09-26 13:37:37 +0800

    init 我的试卷

---
+++ src/examManage/pages/papersManage/mypapers.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mypapers">
+      我的试卷
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style rel="stylesheet/scss" scoped="scoped" lang="scss">
+
+</style>
