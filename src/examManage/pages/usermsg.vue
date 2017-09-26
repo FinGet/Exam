2dddd3c1332cfc548218fee2f2bf4db39d987ecc
Mario <zhangbiaoFT@163.com>
2017-09-26 12:52:14 +0800

    我的信息管理

---
+++ src/examManage/pages/usermsg.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="usermsg">
+      这是用户信息
+      <el-progress :text-inside="true" :stroke-width="18" :percentage="0"></el-progress>
+      <el-progress :text-inside="true" :stroke-width="18" :percentage="70"></el-progress>
+      <el-progress :text-inside="true" :stroke-width="18" :percentage="100" status="success"></el-progress>
+      <el-progress :text-inside="true" :stroke-width="18" :percentage="50" status="exception"></el-progress>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style rel="stylesheet/scss" scoped="scoped" lang="scss">
+
+</style>
