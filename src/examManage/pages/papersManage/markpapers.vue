e4683eebae46a789dbe0c701cf1570d47207c784
Mario <zhangbiaoFT@163.com>
2017-09-27 13:52:24 +0800

    阅卷init

---
+++ src/examManage/pages/papersManage/markpapers.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="markpapers">
+      阅卷
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style rel="stylesheet/scss" scoped="scoped" lang="scss">
+  .markpapers{
+    width: 100%;
+    height:100%;
+    overflow-y: scroll;
+  }
+  .markpapers::-webkit-scrollbar {
+    display:none
+  }
+</style>
