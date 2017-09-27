918faebd40654ba03d830e668fd43903fa065471
Mario <zhangbiaoFT@163.com>
2017-09-27 17:41:05 +0800

    编辑试卷

---
+++ src/examManage/pages/papersManage/editpaper.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="edit">
+      编辑试卷
+      {{id}}
+      <el-button class="pull-right" type="primary" @click="back">返回</el-button>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        id:''
+      }
+    },
+    created() {
+      this.id = this.$route.params.id
+      // console.log(this.$route.params.id)
+    },
+    methods: {
+      // 返回
+      back() {
+        this.$router.go(-1)
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" scoped="scoped" lang="scss">
+
+</style>
