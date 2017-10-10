1954343fe3f9d253ca454b7dbde535562384ecfc
Mario <zhangbiaoFT@163.com>
2017-10-10 17:56:50 +0800

    统一代码缩进

--- src/examManage/login.vue
+++ src/examManage/login.vue
@@ -1,72 +1,72 @@
 <template>
-    <div class="login_page">
-        <div class="login-page-inner">
-            <div class="input-text-wrapper">
-                <div class="login-logo marginB10"><img width="300"  v-lazy="logoSrc" alt="logo"></div>
-                <el-input v-model="userName" placeholder="请输入账号" @keyup.enter="submit"></el-input>
-                <el-input type="password" v-model="password" placeholder="请输入密码" @keyup.enter="submit" class="marginT10"></el-input>
-            </div>
-            <div class="input-text-wrapper marginT30 text-center">
-                <el-button type="primary" @click="submit" class="loginBtn">登录</el-button>
-            </div>
-            <div class="marginT20">
-                <a href="javascript:;">注册账号</a>
-                <a href="javascript:;">忘记密码？</a>
-                <router-link class="note  pull-right" to="/">学生登录</router-link>
-            </div>
-        </div>
-        <div id="canvas"></div>
+  <div class="login_page">
+    <div class="login-page-inner">
+      <div class="input-text-wrapper">
+        <div class="login-logo marginB10"><img width="300"  v-lazy="logoSrc" alt="logo"></div>
+        <el-input v-model="userName" placeholder="请输入账号" @keyup.enter="submit"></el-input>
+        <el-input type="password" v-model="password" placeholder="请输入密码" @keyup.enter="submit" class="marginT10"></el-input>
+      </div>
+      <div class="input-text-wrapper marginT30 text-center">
+        <el-button type="primary" @click="submit" class="loginBtn">登录</el-button>
+      </div>
+      <div class="marginT20">
+        <a href="javascript:;">注册账号</a>
+        <a href="javascript:;">忘记密码？</a>
+        <router-link class="note  pull-right" to="/">学生登录</router-link>
+      </div>
     </div>
+    <div id="canvas"></div>
+  </div>
 </template>
 
 <script>
 //  初始化一些数据
 import { CanvasBackground } from '@/common/js/canvas_bg.js'
 export default {
-    data() {
-        return {
-            userName: '',
-            password: '',
-            logoSrc:require('../common/img/logo.png')
+  data() {
+      return {
+          userName: '',
+          password: '',
+          logoSrc:require('../common/img/logo.png')
+      }
+  },
+  methods: {
+	// 登录
+    submit() {
+      if (this.userName && this.password) {
+        if (this.userName== 'admin' && this.password == '123456') {
+          this.$message({
+            showClose: true,
+            message: '恭喜你，登录成功！',
+            type: 'success'
+          });
+          // 登录成功跳到后台首页
+          this.$router.push('/endhome/index')
+        }else {
+          this.$message({
+            showClose: true,
+            message: '用户名或密码错误！',
+            type: 'warning'
+          });
+          this.password = ''
         }
-    },
-    methods: {
-		// 登录
-        submit() {
-            if (this.userName && this.password) {
-              if (this.userName== 'admin' && this.password == '123456') {
-                this.$message({
-                  showClose: true,
-                  message: '恭喜你，登录成功！',
-                  type: 'success'
-                });
-                // 登录成功跳到后台首页
-                this.$router.push('/endhome/index')
-              }else {
-                this.$message({
-                  showClose: true,
-                  message: '用户名或密码错误！',
-                  type: 'warning'
-                });
-                this.password = ''
-              }
-            } else {
-                this.$message({
-                    showClose: true,
-                    message: '请输入用户名和密码！',
-                    type: 'warning'
-                });
-            }
-        }
-    },
-    mounted() {
-        var bg = new CanvasBackground({
-            canvasContainerID: "canvas",
-            circleColor: "rgba(49,210,142,1)",
-            lineColor: "rgba(49,210,142,1)",
-            canvasOpacity: 0.3
-        });
+      } else {
+          this.$message({
+              showClose: true,
+              message: '请输入用户名和密码！',
+              type: 'warning'
+          });
+      }
     }
+  },
+  mounted() {
+    var bg = new CanvasBackground({
+      canvasContainerID: "canvas",
+      circleColor: "rgba(49,210,142,1)",
+      lineColor: "rgba(49,210,142,1)",
+      canvasOpacity: 0.3
+    });
+  }
 }
 </script>
 
--- src/examManage/pages/usermsg.vue
+++ src/examManage/pages/usermsg.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="usermsg">
-      这是用户信息
-      <el-progress :text-inside="true" :stroke-width="18" :percentage="0"></el-progress>
-      <el-progress :text-inside="true" :stroke-width="18" :percentage="70"></el-progress>
-      <el-progress :text-inside="true" :stroke-width="18" :percentage="100" status="success"></el-progress>
-      <el-progress :text-inside="true" :stroke-width="18" :percentage="50" status="exception"></el-progress>
-    </div>
+  <div class="usermsg">
+    这是用户信息
+    <el-progress :text-inside="true" :stroke-width="18" :percentage="0"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="18" :percentage="70"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="18" :percentage="100" status="success"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="18" :percentage="50" status="exception"></el-progress>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
