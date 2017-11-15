c0bf3820c1e8b565eaeef0baf0b888097906be05
Mario <zhangbiaoFT@163.com>
2017-11-15 16:32:30 +0800

    init connect mongodb

---
+++ server/db.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var dbUrl = 'mongodb://127.0.0.1:27017/examSystem';
+var db = mongoose.connect(dbUrl);
+db.connection.on('error',function(error) {
+	console.log('数据库链接失败：'+ error);
+});
+db.connection.on('connected',function() {
+	console.log('数据库链接成功!');
+});
+db.connection.on('disconnected',function() {
+	console.log('Mongoose connection disconnected'); 
+});
+
+module.exports = mongoose;
---
+++ server/model/user.js
@@ -0,0 +1,8 @@
+var mongoose = require('../db');
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+	username: {type: String}, // 用户账号
+	userpwd: {type: String}, // 密码
+	type: {type: Number} // 账号类型 0:系统管理员 1:教师 2:学生
+})
+module.exports = mongoose.model('User', UserSchema);
