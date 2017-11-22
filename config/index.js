c72e4ac4c41727b515928263035f954868bb400d
Mario <zhangbiaoFT@163.com>
2017-11-22 15:48:27 +0800

    修改代理接口

--- config/index.js
+++ config/index.js
@@ -30,7 +30,7 @@ module.exports = {
     proxyTable: {
         // proxy all requests starting with /api to jsonplaceholder
         '/api': {
-            target: 'http://127.0.0.1:8088/api',
+            target: 'http://127.0.0.1:3000/api',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': ''  // 若target中没有/api、这里又为空，则404；
