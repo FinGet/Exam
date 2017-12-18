0269fbe28bc3758a2fc0c5dc859c915c9dc98762
Mario <zhangbiaoFT@163.com>
2017-12-18 09:50:10 +0800

    model修改

--- server/model/papers.js
+++ server/model/papers.js
@@ -1,13 +1,13 @@
 var mongoose = require('./../db');
 var Schema = mongoose.Schema;
 var PapersSchema = new Schema({
-	teacher: String,
-	name: String,
-    totalPoints: Number,
-    time: String,
-    startTime: Date,
-    endTime: Date,
-    examnum: Number
+    teacher: String, // 老师
+    name: String, // 试卷名称
+    totalPoints: Number, // 总分
+    time: String, // 考试时间
+    startTime: Date, // 开始时间
+    endTime: Date, // 结束时间
+    examnum: Number // 考试人数
 })
 module.exports = mongoose.model('Paper', PapersSchema);
 
--- server/model/user.js
+++ server/model/user.js
@@ -4,15 +4,6 @@ var UserSchema = new Schema({
 	userName: {type: String}, // 用户账号
 	userPwd: {type: String}, // 密码
 	type: {type: Number}, // 账号类型 0:系统管理员 1:教师 2:学生
-	papers: [
-		{
-			name: String,
-        totalPoints: Number,
-        time: String,
-        startTime: Date,
-        endTime: Date,
-        examnum: Number
-		}
-	]
+	papers: []
 })
 module.exports = mongoose.model('User', UserSchema);
