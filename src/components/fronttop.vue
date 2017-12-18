0749d86543f8baa26e7211fec9a3949210b2eee0
Mario <zhangbiaoFT@163.com>
2017-12-18 14:52:27 +0800

    学生端头部组件

---
+++ src/components/fronttop.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="fronttop">
+      <div class="top"></div>
+      <div class="top-user">
+        <ul class="user-info">
+          <li>姓名:张三</li>
+          <li>学号:1235621</li>
+          <li>性别:男</li>
+        </ul>
+        <el-button type="danger" size="mini" class="login-out">退出</el-button>
+      </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style rel="stylesheet/scss" scoped="scoped" lang="scss">
+  .top {
+    width: 100%;
+    height:110px;
+    margin:0 auto;
+    background: url("../common/img/banner.png") center center;
+  }
+  .top-user{
+    width: 100%;
+    height: 30px;
+    line-height: 30px;
+    margin:0 auto;
+    font-size: 16px;
+    background: #08829f;
+    .user-info{
+      float: left;
+      color: #fff;
+      line-height:30px;
+      li {
+        float: left;
+        margin-left: 20px;
+      }
+    }
+    .login-out{
+      float: right;
+      margin-right: 50px;
+      margin-top: 4px;
+    }
+  }
+</style>
