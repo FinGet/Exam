e4b6012cbcbe5c61bbbe9827cfb95a8b261ab9ed
Mario <zhangbiaoFT@163.com>
2017-12-19 10:20:20 +0800

    删除试题管理、组织考试

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -13,22 +13,9 @@
                 <el-menu-item index="/endhome/markpapers">阅卷</el-menu-item>
             </el-submenu>
             <el-submenu index="3">
-              <template slot="title"><i class="fa-icon fa fa-bullhorn"></i>组织考试</template>
-              <el-menu-item index="3-1">已组织的考试</el-menu-item>
-              <el-menu-item index="3-2">发布公告</el-menu-item>
-            </el-submenu>
-            <el-submenu index="4">
               <template slot="title"><i class="fa-icon fa fa-group"></i>学生成绩管理</template>
-              <el-menu-item index="4-1">全部学生成绩</el-menu-item>
-              <el-menu-item index="4-2">班级成绩</el-menu-item>
-            </el-submenu>
-            <el-submenu index="5">
-              <template slot="title"><i class="el-icon-edit"></i>试题管理</template>
-              <el-menu-item index="5-1">单选题管理</el-menu-item>
-              <el-menu-item index="5-2">多选题管理</el-menu-item>
-              <el-menu-item index="5-3">填空题管理</el-menu-item>
-              <el-menu-item index="5-4">判断题管理</el-menu-item>
-              <el-menu-item index="5-5">问答题管理</el-menu-item>
+              <el-menu-item index="3-1">全部学生成绩</el-menu-item>
+              <el-menu-item index="3-2">班级成绩</el-menu-item>
             </el-submenu>
             <el-menu-item index="/endhome/user"><i class="fa-icon fa fa-user"></i>我的信息管理</el-menu-item>
           </el-menu>
