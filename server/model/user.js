6d22087f54b08104d8a853dc974f3ee184bfbc34
Mario <zhangbiaoFT@163.com>
2017-12-19 16:17:48 +0800

    用户模型

--- server/model/user.js
+++ server/model/user.js
@@ -1,7 +1,8 @@
 var mongoose = require('./../db');
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-	userName: {type: String}, // 用户账号
+  userId: {type: String}, // 用户id
+	userName: {type: String}, // 用户名称
 	userPwd: {type: String}, // 密码
 	type: {type: Number}, // 账号类型 0:系统管理员 1:教师 2:学生
 	papers: []
