ac7fc41f6d4febaff9c1ff861ac4a1a051f8a5b4
FinGet <zhangbiaoFT@163.com>
2018-04-17 15:56:58 +0800

    学生model

--- server/model/student.js
+++ server/model/student.js
@@ -4,8 +4,8 @@ var StudentSchema = new Schema({
     userId: Number, // 学号
     userName: String, // 用户名
     passWord: String, // 密码
-    grade: Number, // 年级 1~6 分别代表一年级到六年级
-    class: Number, // 班级
+    grade: String, // 年级 1~6 分别代表一年级到六年级
+    class: String, // 班级
     exams:[{ // 参加的考试
       _paper:{ type: Schema.Types.ObjectId, ref: 'Paper' },
       date: Date,
