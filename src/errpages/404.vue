34769f84fd3785922fa1ba0b36303ea11b113834
FinGet <zhangbiaoFT@163.com>
2018-05-04 11:45:56 +0800

    测试-修改文字

--- src/errpages/404.vue
+++ src/errpages/404.vue
@@ -11,7 +11,7 @@
         <div class="bullshit__oops">Hello 404!</div>
         <div class="bullshit__info">&copy;版权所有<a class='link-type' href='http://finget.github.io' target='_blank'>FinGet</a></div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
+        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回上一页</div>
         <a href="javascript:;" class="bullshit__return-home" @click="goBack">返回上一页</a>
       </div>
     </div>
